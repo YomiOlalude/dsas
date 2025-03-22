d26d518548a0e2977bb721024be5e3d531f07a98
Yomi Olalude <yomi.olalude@gmail.com>
2025-03-22 16:08:16 +0100

    finished tuts

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -4,7 +4,7 @@ def add_up_to(num):
     """
     if num <= 0:
         return 0
-    return num + add_up_to(num-1)
+    return num + add_up_to(num - 1)
 
 
 def countdown(num):
@@ -14,7 +14,7 @@ def countdown(num):
     print(num)
     if num <= 0:
         return "Finished"
-    return countdown(num-1)
+    return countdown(num - 1)
 
 
 def factorial(num):
@@ -27,7 +27,33 @@ def factorial(num):
     if num == 1:
         return 1
 
-    return num * factorial(num-1)
+    return num * factorial(num - 1)
+
+
+def fibonacci_array(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    result = fibonacci_array(n - 1)
+    result.append(result[-1] + result[-2])
+    return result
+
+
+def fibonacci_add(n, memo=None):
+    if memo is None:
+        memo = {}
+
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+
+    memo[n] = fibonacci_add(n - 1, memo) + fibonacci_add(n - 2, memo)
+    return memo[n]
 
 
 def reverse_string(string):
