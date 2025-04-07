09b50cf14e0f750bc2a299ead4080a5d9d8dfacd
Yomi Olalude <yomi.olalude@gmail.com>
2025-04-07 22:59:32 +0200

    add bst js

--- javascript/data-structures/5_binary-search-tree.js
+++ javascript/data-structures/5_binary-search-tree.js
@@ -13,13 +13,13 @@ class BST {
 
 	insert(value) {
 		let newNode = new Node(value);
-    
-    if (!this.root) {
-      this.root = newNode;
-      return this
-    }
-    
-    let current = this.root;
+
+		if (!this.root) {
+			this.root = newNode;
+			return this;
+		}
+
+		let current = this.root;
 
 		while (true) {
 			if (value === current.value) {
@@ -45,4 +45,107 @@ class BST {
 			}
 		}
 	}
+
+	find(value) {
+		if (!this.root) {
+			return null;
+		}
+
+		current = this.root;
+
+		while (current) {
+			if (value < current.value) {
+				current = current.left;
+			} else if (value > current.value) {
+				current = current.right;
+			} else {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bfs() {
+		let result = [];
+		let queue = [this.root];
+		let current = this.root;
+
+		while (queue.length > 0) {
+			current = queue.pop();
+			result.push(current.value);
+
+			if (current.left) {
+				queue.push(current.left);
+			}
+			if (current.right) {
+				queue.push(current.right);
+			}
+		}
+
+		return result;
+	}
+
+	dfs_preorder() {
+		let result = [];
+
+		function traverse(node) {
+			result.push(node.value);
+			if (node.left) {
+				traverse(node.left);
+			}
+			if (node.right) {
+				traverse(node.right);
+			}
+		}
+
+		traverse(this.root);
+		return result;
+	}
+
+	dfs_postorder() {
+		let result = [];
+
+		function traverse(node) {
+			if (node.left) {
+				traverse(node.left);
+			}
+			if (node.right) {
+				traverse(node.right);
+			}
+			result.push(node.value);
+		}
+
+		traverse(this.root);
+		return result;
+	}
+
+	dfs_inorder() {
+		let result = [];
+
+		function traverse(node) {
+			if (node.left) {
+				traverse(node.left);
+			}
+			result.push(node.value);
+			if (node.right) {
+				traverse(node.right);
+			}
+		}
+
+		traverse(this.root);
+		return result;
+	}
 }
+
+let bst = new BST();
+
+bst.insert(6);
+bst.insert(5);
+bst.insert(4);
+bst.insert(1);
+bst.insert(8);
+bst.insert(9);
+bst.insert(3);
+bst.insert(10);
+
+console.log(bst.bfs());
